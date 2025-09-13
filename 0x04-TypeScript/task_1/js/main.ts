9b95b3fd1f5440840f34e997c95546a615849241
Elias Derbew <eliasderbew1@gmail.com>
2025-09-13 23:10:38 +0300

    lets build a teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: "Alx",
+  lastName: "Ethiopia",
+  fullTimeEmployee: true,
+  location: "Addis Ababa, Ethiopia",
+  contract: true,
+};
+
+console.log(teacher3)
