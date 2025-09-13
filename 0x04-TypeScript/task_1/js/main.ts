1e729fd228e940f62d4e45b68cd8811806aaf5be
Elias Derbew <eliasderbew1@gmail.com>
2025-09-13 23:37:21 +0300

    Director interface extends Teachers property

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,5 +1,3 @@
-//  task 1
-
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -9,29 +7,6 @@ interface Teacher {
   [key: string]: any;
 }
 
-//  task 2
-
 interface Directors extends Teacher {
   numberOfReports: number;
 }
-
-// const teacher3: Teacher = {
-//   firstName: "Alx",
-//   lastName: "Ethiopia",
-//   fullTimeEmployee: true,
-//   location: "Addis Ababa, Ethiopia",
-//   contract: true,
-// };
-
-// console.log(teacher3);
-
-
-// const director1: Directors = {
-//   firstName: "Elias",
-//   lastName: "Belay",
-//   fullTimeEmployee: false,
-//   location: "Addis Ababa, Ethiopia",
-//   numberOfReports: 17,
-// };
-
-// console.log(director1);
