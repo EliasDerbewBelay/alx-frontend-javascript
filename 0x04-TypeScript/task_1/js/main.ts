67b60b8267da131244be7ea9e69b1565e5f662bf
Elias Derbew <eliasderbew1@gmail.com>
2025-09-14 00:15:29 +0300

    print teachers name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,15 +10,22 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
-// Define the interface
+// Interface for the function
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  ({ firstName, lastName }: { firstName: string; lastName: string }): string;
 }
 
-// Implement as a regular function
-function printTeacher(firstName: string, lastName: string): string {
+// Function implementation
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-// Example usage
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
+// Example
+console.log(printTeacher({ firstName: "John", lastName: "Doe" })); // J. Doe
+// Output: J. Doe
