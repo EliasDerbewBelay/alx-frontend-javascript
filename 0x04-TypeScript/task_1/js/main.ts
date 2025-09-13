64bb099b38c29d22a2cbf15477b1119ee6acc2e2
Elias Derbew <eliasderbew1@gmail.com>
2025-09-14 00:00:24 +0300

    print teachers name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,7 +15,7 @@ interface printTeacherFunction {
 }
 
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
+  return `${firstName[0]}. ${lastName}`;
 }
 
 console.log(printTeacher("John", "Doe"));
