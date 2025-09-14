6db1133d8be55cb3ca2c747cbd00a8068215e72b
Elias Derbew <eliasderbew1@gmail.com>
2025-09-14 15:11:47 +0300

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,22 +1,16 @@
 /// <reference path="./crud.d.ts" />
 
-import { RowID, RowElement } from "../interface";
+import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
-// Original row object
 const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
 };
 
-// Insert row
 const newRowID: RowID = CRUD.insertRow(row);
 
-// Updated row using object spread
 const updatedRow: RowElement = { ...row, age: 23 };
 
-// Update row
 CRUD.updateRow(newRowID, updatedRow);
-
-// Delete row
 CRUD.deleteRow(newRowID);
